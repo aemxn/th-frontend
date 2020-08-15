60f4bf0379723e23b36c05c03ad0172a619314bb
AIman Baharum <aiman.baharum@outlook.com>
2020-08-15 21:57:32 +0800

    removed prototype axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
-import Axios from './http';
 import router from './router';
 import Utils from './utils';
 
-Vue.prototype.$http = Axios;
 Vue.prototype.$utils = Utils;
 
 Vue.use(BootstrapVue);
